08502f57c30b57b30bfdcb78efabfcd12a228246
Steve Hannah <steve@weblite.ca>
2014-03-24 16:58:45 -0700

    Added view_related_record action as first step toward making xataface applications more convex.  Still need to connect these up to the related list and add editing functionality.

---
+++ actions/view_related_record.php
@@ -0,0 +1,30 @@
+<?php
+class dataface_actions_view_related_record {
+	function handle($params){
+		$app = Dataface_Application::getInstance();
+		$query =& $app->getQuery();
+		
+		$related_record = df_get_record_by_id($query['-related-record-id']);
+		if ( !$related_record || PEAR::isError($related_record) ){
+			$this->out_404();
+		}
+		$app->_conf['orig_permissions'] = $related_record->getPermissions();
+		Dataface_PermissionsTool::addContextMask($related_record);
+		$perms = $related_record->getPermissions();
+		if ( !@$perms['view'] ){
+			return Dataface_Error::permissionDenied('You don\'t have permission to view this record.');
+		}
+		
+		$query['-relationship'] = $related_record->_relationship->getName();
+		Dataface_JavascriptTool::getInstance()->import('xataface/actions/view_related_record.js');
+		df_display(
+			array('related_record' => $related_record ), 
+			'xataface/actions/view_related_record.html'
+		);
+		
+	}
+	
+	function out_404(){
+		echo "Not found";
+	}
+}
---
+++ js/xataface/actions/view_related_record.js
@@ -0,0 +1,7 @@
+//require <jquery-ui.min.js>
+//require-css <xataface/actions/view_related_records.css>
+(function(){
+	var $ = jQuery;
+	
+	$('.subrecord-tabs').tabs();
+})();
