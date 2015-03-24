41116a9f868018879ecf66dd8b68c3eb340ee802
Steve Hannah <steve@weblite.ca>
2015-03-24 10:48:27 -0700

    Close sessions before getBlob action
    This should increase performance for multiple simultaneous blob requests from same client.

--- actions/getBlob.php
+++ actions/getBlob.php
@@ -4,6 +4,9 @@ class dataface_actions_getBlob {
 	function handle(&$params){
 	
 		$app =& Dataface_Application::getInstance();
+		if (!@$app->_conf['blob_keep_sessions_open']){
+			@session_write_close();
+		}
 		$query =& $app->getQuery();
 		$app->_handleGetBlob($query);
 		exit;
