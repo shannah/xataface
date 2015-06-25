ecaa3de3c0625fcadf8cb7f4a5847a0323917d25
Steve Hannah <steve@weblite.ca>
2015-06-24 21:13:05 -0700

    Removed typo from previous commit

--- Dataface/Record.php
+++ Dataface/Record.php
@@ -18,7 +18,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *-------------------------------------------------------------------------------
  */
- di
 
 import( 'Dataface/Table.php');
 import( 'Dataface/RelatedRecord.php');
