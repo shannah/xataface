8dbcb0e31337ebe77cd229c0d541d9447426d9b0
Steve Hannah <steve@weblite.ca>
2017-08-14 12:40:50 -0700

    Changed order of parameters in xf_db_real_escape_string to match old mysql extension.  Fixed issues with mysql_escape_string and mysql_real_escape_string in mysqli driver related.

--- xf/db/drivers/mysqli.php
+++ xf/db/drivers/mysqli.php
@@ -9,8 +9,8 @@ function xf_db_connect($host,$user,$pass){
 }
 function xf_db_connect_errno(){ return mysqli_connect_errno();}
 function xf_db_connect_error(){ return mysqli_connect_error();}
-function xf_db_query($sql, $conn=null){ 
-	return mysqli_query($conn, $sql); 
+function xf_db_query($sql, $conn=null){
+	return mysqli_query($conn, $sql);
 }
 function xf_db_error($link=null){
 	if ( $link === null ){
@@ -19,24 +19,24 @@ function xf_db_error($link=null){
 		return mysqli_error($link);
 	}
 }
-function xf_db_errno($link=null){ 
+function xf_db_errno($link=null){
 	if ( $link === null ){
 		return mysqli_errno();
 	}
-	return mysqli_errno($link); 
+	return mysqli_errno($link);
 }
-function xf_db_escape_string($unescaped_string){ return mysqli_escape_string($unescaped_string); }
-function xf_db_real_escape_string($link, $unescaped_string){ return mysqli_real_escape_string($link, $unescaped_string); }
+function xf_db_escape_string($unescaped_string){ return mysqli_escape_string(df_db(), $unescaped_string); }
+function xf_db_real_escape_string($unescaped_string, $link){ return mysqli_real_escape_string($link, $unescaped_string); }
 function xf_db_fetch_array($result){ return mysqli_fetch_array($result); }
 function xf_db_fetch_assoc($result){ return mysqli_fetch_assoc($result); }
 function xf_db_fetch_object($result){ return mysqli_fetch_object($result); }
 function xf_db_fetch_row($result){ return mysqli_fetch_row($result); }
-function xf_db_select_db($dbname, $link=null){ 
-	
-	return mysqli_select_db($link, $dbname); 
+function xf_db_select_db($dbname, $link=null){
+
+	return mysqli_select_db($link, $dbname);
 }
 function xf_db_free_result($result){ return mysqli_free_result($result);}
-function xf_db_affected_rows($link=null){ 
+function xf_db_affected_rows($link=null){
 	if ( $link === null ){
 		return mysqli_affected_rows($link);
 	}
@@ -44,26 +44,26 @@ function xf_db_affected_rows($link=null){
 }
 function xf_db_fetch_lengths($result){ return mysqli_fetch_lengths($result);}
 function xf_db_num_rows($result){ return mysqli_num_rows($result);}
-function xf_db_insert_id($link=null){ 
+function xf_db_insert_id($link=null){
 	if ( $link === null ){
 		return mysqli_insert_id();
 	}
 	return mysqli_insert_id($link);
 }
 function xf_db_data_seek($result, $offset){ return mysqli_data_seek($result, $offset);}
-function xf_db_character_set_name($link=null){ 
+function xf_db_character_set_name($link=null){
 	if ( $link === null ){
 		return mysqli_character_set_name();
 	}
 	return mysqli_character_set_name($link);
 }
-function xf_db_close($link=null){ 
+function xf_db_close($link=null){
 	if ( $link === null ){
 		return mysqli_close();
 	}
 	return mysqli_close($link);
 }
-function xf_db_get_server_info($link=null){ 
+function xf_db_get_server_info($link=null){
 	if ( $link === null ){
 		return mysqli_get_server_info();
 	}
