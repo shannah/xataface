8f0d6d03be7d1f239b4bdeb4c25f5d7d99249b69
Steve Hannah <steve@weblite.ca>
2019-04-13 10:46:07 -0700

    Added BaseAction that makes it easier to implement custom actions with exception handling for web services.

---
+++ xf/actions/BaseAction.php
@@ -0,0 +1,30 @@
+<?php
+namespace xf\actions;
+
+class BaseAction {
+    public function handle($params) {
+        $app = Dataface_Application::getInstance();
+        $res = null;
+        try {
+            $res = $this->handleImpl($params);
+
+        } catch (Exception $ex) {
+            if ($ex->getCode() === 401) {
+                return Dataface_Error::permissionDenied('Permission is denied');
+            }
+
+            $res = array(
+                'code' => 500,
+                'errorCode' => $ex->getCode(),
+                'errorMessage' => $ex->getMessage(),
+                'message' => 'Publish attempt failed'
+            );
+        }
+        header('Content-type: application/json; charset="'.$app->_conf['oe'].'"');
+        echo json_encode($res);
+    }  
+
+    public function handleImpl($params) {
+
+    }
+}
