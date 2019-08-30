356864dceb11d5dd792b42c7df94d32c3d797355
Steve Hannah <steve@weblite.ca>
2019-08-30 16:01:02 -0700

    Removed tools from bin that have been copied to scaffold_template.

--- tools/mysql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-scaffolddir="$SCRIPTPATH/..";
-mysql --socket="$scaffolddir"/tmp/mysql.sock -u `php print_config_var.php _database.user` `php print_config_var.php _database.name`
