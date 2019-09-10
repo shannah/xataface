1b009ce36c04338b7f60aec19580840fa64aa7ad
Steve Hannah <steve@weblite.ca>
2019-09-10 15:58:15 -0700

    Removed files copied into wrong directory

--- scaffold_template/mysqldump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export PATH=/Applications/XAMPP/xamppfiles/bin:$PATH
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-scaffolddir="$SCRIPTPATH/..";
-ACMD="$1"
-mysqldump --socket="$scaffolddir"/tmp/mysql.sock -u `php $SCRIPTPATH/print_config_var.php _database.user` `php $SCRIPTPATH/print_config_var.php _database.name`
-ERROR=$?
-exit $ERROR
--- scaffold_template/xfshell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-cd $SCRIPTPATH/../www
-sh $SCRIPTPATH/php.sh -a "$@"
