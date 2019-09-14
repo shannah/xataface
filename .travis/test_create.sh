84a289fb2c0110b895269d33c0b7b1490ae575f2
Steve Hannah <steve@weblite.ca>
2019-09-14 14:49:04 -0700

    Added some logging to travis

--- .travis/test_create.sh
+++ .travis/test_create.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 PROJECT_DIR=`pwd`
 cd ..
 mkdir tests
 cd tests
 set -e
+
+function print_errors() {
+    echo "mysql-errors.log:\n"
+    cat $PROJECT_DIR/../tests/testapp/log/mysql-errors.log
+}
+trap print_errors EXIT
 php $PROJECT_DIR/tools/create.php testapp
