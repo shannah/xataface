20e10f610d3f0e8cbe0b664c68ce8629f7fa3cde
Steve Hannah <steve@weblite.ca>
2019-09-14 16:28:07 -0700

    More travis

--- tools/create.php
+++ tools/create.php
@@ -439,7 +439,7 @@ END;
         $mysql_server = $this->bin_dir() . DIRECTORY_SEPARATOR . 'mysql.server.sh';
         $mysql = $this->bin_dir() . DIRECTORY_SEPARATOR . 'mysql.sh';
         echo "Starting mysql server...";
-        passthru('bash '.escapeshellarg($mysql_server).' start', $res);
+        exec('bash '.escapeshellarg($mysql_server).' start', $buffer, $res);
         if ($res !== 0) {
             fwrite(STDERR, "Failed to start mysql server.\n");
             exit(1);
