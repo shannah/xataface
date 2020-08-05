3fa566ece8a668df62f12ac71600224c6b00b9cd
Steve Hannah <steve@weblite.ca>
2020-08-05 06:35:48 -0700

    Removed debugging print statements from mysql.sh. Added exclusions for xataface views in mysqldump.sh

--- scaffold_template/bin/mysql.sh
+++ scaffold_template/bin/mysql.sh
@@ -5,13 +5,13 @@ scaffolddir="$SCRIPTPATH/..";
 ACMD="$1"
 case $ACMD in
 init)
-    echo "Doing mysql init"
+    #echo "Doing mysql init"
     mysql --socket="$scaffolddir"/tmp/mysql.sock
     ERROR=$?
 ;;
 *)
-    echo "Doing regular mysql"
-    mysql --socket="$scaffolddir"/tmp/mysql.sock -u `php $SCRIPTPATH/print_config_var.php _database.user` `php $SCRIPTPATH/print_config_var.php _database.name`
+    #echo "Doing regular mysql"
+    mysql --socket="$scaffolddir"/tmp/mysql.sock -u `php $SCRIPTPATH/print_config_var.php _database.user` `php $SCRIPTPATH/print_config_var.php _database.name` "$@"
     ERROR=$?
 ;;
 esac
