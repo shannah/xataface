c56317de209e0b5cf52d0eee1bb55df5c889e54b
Steve Hannah <steve@weblite.ca>
2021-04-02 06:27:41 -0700

    Fixed regression in smarty compiler trying to fix deprecation warnings.

--- lib/Smarty/Smarty_Compiler.class.php
+++ lib/Smarty/Smarty_Compiler.class.php
@@ -563,9 +563,12 @@ class Smarty_Compiler extends Smarty {
 
             case 'php':
                 /* handle folded tags replaced by {php} */
-                $tmpKey = key($this->_folded_blocks);
-                $block = current($this->_folded_blocks);
+                
                 //list(, $block) = each($this->_folded_blocks);
+                //Removed each(..) because deprecation warnings in PHP 7/8.
+                // Replaced with current();next(); which seems to do the job.
+                $block = current($this->_folded_blocks);
+                next($this->_folded_blocks);
                 $this->_current_line_no += substr_count($block[0], "\n");
                 /* the number of matched elements in the regexp in _compile_file()
                    determins the type of folded tag that was found */
